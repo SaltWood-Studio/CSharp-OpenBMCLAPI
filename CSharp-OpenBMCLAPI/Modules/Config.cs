d8e1b3b7173141984dbce463981643add7740b3f
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-03-02 22:51:03 +0800

    预留了 config，支持在 token 失效之前进行自动刷新

---
+++ CSharp-OpenBMCLAPI/Modules/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOpenBMCLAPI.Modules
+{
+    public class Config
+    {
+        // 指示 token 应当在距离其失效前的多少毫秒进行刷新
+        public int refreshTokenTime;
+
+        public Config()
+        {
+            this.refreshTokenTime = 1800000;
+        }
+    }
+}
--- CSharp-OpenBMCLAPI/Modules/TokenManager.cs
+++ CSharp-OpenBMCLAPI/Modules/TokenManager.cs
@@ -12,6 +12,12 @@ namespace CSharpOpenBMCLAPI.Modules
     internal class TokenManager
     {
         private readonly string CLUSTER_ID, CLUSTER_SECRET;
+        private Task? _updateTask;
+
+        private Token token;
+        private object _writeLocker = new object();
+
+        public Token Token { get { lock (_writeLocker) return token; } }
 
         public TokenManager(ClusterInfo info)
         {
@@ -44,9 +50,25 @@ namespace CSharpOpenBMCLAPI.Modules
 
             resp = await client.PostAsync($"openbmclapi-agent/token", httpContent);
             string tokenJson = await resp.Content.ReadAsStringAsync();
-            Token token = JsonConvert.DeserializeObject<Token>(tokenJson);
+
+            // 锁定 token，以免其他线程同时进行读取出现问题
+            lock (this._writeLocker)
+            {
+                this.token = JsonConvert.DeserializeObject<Token>(tokenJson).ThrowIfNull();
+
+                this._updateTask = Task.Run(() =>
+                {
+                    Thread.Sleep(this.token.ttl - SharedData.Config.refreshTokenTime);
+                    RefreshToken();
+                });
+            }
 
             return token;
+        }
+
+        public void RefreshToken()
+        {
+            FetchToken().Wait();
         }
     }
 }
