25f6e8121e0c512a65691bec6ba297a5257a2b50
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-04-04 00:01:27 +0800

    todo: 更多 Https 服务方式以取代低效的 ASP.NET

---
+++ CSharp-OpenBMCLAPI/Modules/WebServer/HttpContentPacket.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpOpenBMCLAPI.Modules.WebServer
+{
+    public class HttpContentPacket
+    {
+        public HttpContentPacket()
+        {
+
+        }
+
+        [Obsolete($"Please use {nameof(HttpContentPacket.Create)} instead.")]
+        public HttpContentPacket(byte[] data)
+        {
+
+        }
+
+        public static HttpContentPacket? Create(byte[] bytes)
+        {
+            // TODO: HttpContentPacket
+            return null;
+        }
+    }
+}
