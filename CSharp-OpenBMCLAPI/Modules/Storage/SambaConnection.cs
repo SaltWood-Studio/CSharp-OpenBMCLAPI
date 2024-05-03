d95f6a118f0d1bc1f4e840e33c6f789f6dc9ccce
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-05-03 21:44:04 +0800

    feat: 从代码层面添加对 Samba 文件源的兼容

---
+++ CSharp-OpenBMCLAPI/Modules/Storage/SambaConnection.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpOpenBMCLAPI.Modules.Storage
+{
+    public class SambaConnection : IDisposable
+    {
+        const int logonProvider = 0; // Default
+        const int logonType = 9; // New credentials
+        private bool disposed;
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool LogonUser(string pszUsername, string pszDomain, string pszPassword, int dwLogonType, int dwLogonProvider, ref nint phToken);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        extern static bool CloseHandle(nint handle);
+
+        [DllImport("advapi32.DLL")]
+        static extern bool ImpersonateLoggedOnUser(nint hToken);
+
+        [DllImport("advapi32.DLL")]
+        static extern bool RevertToSelf();
+
+        public SambaConnection(string username, string password, string address)
+        {
+            nint existingTokenHandle = new nint(0);
+            nint duplicateTokenHandle = new nint(0);
+
+            try
+            {
+                bool impersonated = LogonUser(username, address, password, logonType, logonProvider, ref existingTokenHandle);
+
+                if (impersonated)
+                {
+                    if (!ImpersonateLoggedOnUser(existingTokenHandle))
+                    {
+                        int errorCode = Marshal.GetLastWin32Error();
+                        throw new Exception($"线程无法以 {username} 的身份登录并获取权限！错误代码：{errorCode}");
+                    }
+                }
+                else
+                {
+                    int errorCode = Marshal.GetLastWin32Error();
+                    throw new Exception($"登录失败！错误代码：{errorCode}");
+                }
+            }
+            finally
+            {
+                if (existingTokenHandle != nint.Zero)
+                {
+                    CloseHandle(existingTokenHandle);
+                }
+
+                if (duplicateTokenHandle != nint.Zero)
+                {
+                    CloseHandle(duplicateTokenHandle);
+                }
+            }
+        }
+
+        ~SambaConnection()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // 释放大型托管资源
+                }
+                // 释放非托管资源
+                RevertToSelf();
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void CheckIfDisposed()
+        {
+            if (this.disposed)
+            {
+                throw new ObjectDisposedException($"对象 {this} 已被清理。");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"<{this.GetType().FullName} object, HashCode = {this.GetHashCode()}>";
+        }
+    }
+}
